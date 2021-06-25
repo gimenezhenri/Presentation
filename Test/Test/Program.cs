54e1e8ffb01cfd8dfe1a6ec113484884a3437087
sioslam alondres <sioslam.alondres@gmail.com>
2021-06-25 20:00:19 +0200

    Bonjour le monde

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -7,6 +7,7 @@ namespace Test
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Bonjour  le monde !");
             Console.ReadKey();
         }
     }
